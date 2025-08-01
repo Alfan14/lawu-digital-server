cb3c360afcb9e1213aa4095c3d1e6a5addd61910
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-08-02 00:17:11 +0700

    update cloudinaryControllers

--- controllers/cloudinaryControllers.mjs
+++ controllers/cloudinaryControllers.mjs
@@ -21,7 +21,7 @@ cloudinary.config({
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.array('image'), (req, res) => {
+router.post('/upload', upload.single('image'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
