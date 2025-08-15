c2b6055662639bafe95396a937099f9b52161e16
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2025-08-15 21:46:22 +0700

    update news get slug routes

--- routes/adminRoute.mjs
+++ routes/adminRoute.mjs
@@ -29,7 +29,7 @@ router.get('/get/:id', async (req, res) => {
 router.get('/get/:slug', async (req, res) => {
   const slug = parseInt(req.params.slug)
 
-  pool.query('SELECT * FROM news_post WHERE slug = $1', [slug], (error, results) => {
+  pool.query('SELECT * FROM news_post AS np WHERE np.slug = $1', [slug], (error, results) => {
     if (error) {
       throw error
     }
